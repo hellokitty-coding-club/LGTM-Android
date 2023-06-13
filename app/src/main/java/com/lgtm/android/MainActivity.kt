ec6397a47217e3b0838745d1911264a18c4ead6e
kxxhyorim <rlagyfla0709@gmail.com>
2023-06-14 03:56:49 +0900

    [CHORE] MainActivity Compose -> DataBinding (#2)

--- app/src/main/java/com/lgtm/android/MainActivity.kt
+++ app/src/main/java/com/lgtm/android/MainActivity.kt
@@ -1,46 +1,12 @@
 package com.lgtm.android
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.lgtm.android.ui.theme.LGTMAndroidTheme
+import com.lgtm.android.common_ui.base.BaseActivity
+import com.lgtm.android.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            LGTMAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LGTMAndroidTheme {
-        Greeting("Android")
-    }
 }
