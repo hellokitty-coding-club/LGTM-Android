2fa3b336fb2d76ce11ba69e6510b1e05631b1de2
kxxhyorim <rlagyfla0709@gmail.com>
2023-06-15 15:37:07 +0900

    [CHORE] Github Action 테스트를 위해 고의로 발생시킨 에러 수정 (#2)

--- feature/auth/src/main/java/com/lgtm/android/auth/AuthActivity.kt
+++ feature/auth/src/main/java/com/lgtm/android/auth/AuthActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth // github action build fail test
+        setContentView(R.layout.activity_auth)
     }
 }
