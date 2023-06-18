8b4991df205c4b3d9d9206e7585109ef05ecf794
kxxhyorim <rlagyfla0709@gmail.com>
2023-06-18 20:05:34 +0900

    [CHORE] Init feature:splash module (#5)

---
+++ feature/splash/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("android")
+    id("com.android.library")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.lgtm.android.splash"
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":common-ui"))
+
+    implementation(libs.bundles.androidx.ui.foundation)
+    implementation(libs.constraintlayout)
+    implementation(libs.hilt)
+    kapt(libs.hilt.kapt)
+    implementation(libs.bundles.basic.test)
+}
