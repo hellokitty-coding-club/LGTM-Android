0dfcd1d19cb60c0d471b5796b79c03aee207aa00
kxxhyorim <rlagyfla0709@gmail.com>
2023-06-19 19:37:37 +0900

    [ADD] RetrofitServiceModule 추가 (#6)

---
+++ app/src/main/java/com/lgtm/android/di/RetrofitServiceModule.kt
@@ -0,0 +1,20 @@
+package com.lgtm.android.di
+
+import com.lgtm.android.data.remote.service.IntroService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitServiceModule {
+
+    @Provides
+    @Singleton
+    fun providesIntroService(retrofit: Retrofit): IntroService =
+        retrofit.create(IntroService::class.java)
+
+}
