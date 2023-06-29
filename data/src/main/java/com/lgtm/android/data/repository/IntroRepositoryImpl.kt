d5ca0fb122c33db7c868adef84ffaa83617129de
kxxhyorim <rlagyfla0709@gmail.com>
2023-06-29 20:15:00 +0900

    [FEAT] RepositoryImpl / try-catch 활용한 네트워크 에러처리 (#12)

--- data/src/main/java/com/lgtm/android/data/repository/IntroRepositoryImpl.kt
+++ data/src/main/java/com/lgtm/android/data/repository/IntroRepositoryImpl.kt
@@ -1,8 +1,6 @@
 package com.lgtm.android.data.repository
 
 import com.lgtm.android.data.remote.datasource.IntroDataSource
-import com.lgtm.android.data.remote.model.response.BaseResponse
-import com.lgtm.android.data.remote.model.response.IntroDTO
 import com.lgtm.domain.entity.response.IntroVO
 import com.lgtm.domain.repository.IntroRepository
 import javax.inject.Inject
@@ -11,9 +9,13 @@ class IntroRepositoryImpl @Inject constructor(
     private val introDataSource: IntroDataSource
 ) : IntroRepository {
     override suspend fun getIntro(): Result<IntroVO> {
-        val response: BaseResponse<IntroDTO> = introDataSource.getIntro()
-        if (response.success)
-            return Result.success(response.data.toVO())
-        return Result.failure(Exception(response.message))
+        try {
+            val response = introDataSource.getIntro()
+            if (response.success)
+                return Result.success(response.data.toVO())
+            return Result.failure(Exception(response.message))
+        } catch (e: Exception) {
+            return Result.failure(e)
+        }
     }
 }
