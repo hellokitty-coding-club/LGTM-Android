09314c3ffde7046c484cedcc845f335f3cb48060
kxxhyorim <rlagyfla0709@gmail.com>
2023-07-07 20:09:10 +0900

    [UI] 이용약관 동의 checkbox 로직 구현 (#17)

--- feature/auth/src/main/java/com/lgtm/android/auth/ui/signup/TermsFragment.kt
+++ feature/auth/src/main/java/com/lgtm/android/auth/ui/signup/TermsFragment.kt
@@ -12,6 +12,29 @@ class TermsFragment : BaseFragment<FragmentTermsBinding>(R.layout.fragment_terms
     private val signUpViewModel by activityViewModels<SignUpViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
+        setOnClickListener()
     }
+
+    private fun setOnClickListener() {
+        binding.cbTermsAll.setOnClickListener {
+            val isChecked = binding.cbTermsAll.isChecked
+            binding.cbTermsMarketing.isChecked = isChecked
+            binding.cbTermsPrivacy.isChecked = isChecked
+            binding.cbTermsService.isChecked = isChecked
+        }
+
+        // 약관 및 정책
+        binding.cbTermsService.setOnClickListener { setTermsAll() }
+        // 개인정보 처리 방침
+        binding.cbTermsPrivacy.setOnClickListener { setTermsAll() }
+        // 이벤트
+        binding.cbTermsMarketing.setOnClickListener { setTermsAll() }
+    }
+
+    private fun setTermsAll() {
+        binding.cbTermsAll.isChecked =
+            binding.cbTermsMarketing.isChecked && binding.cbTermsPrivacy.isChecked && binding.cbTermsService.isChecked
+    }
+
+
 }
