acdc097e383e96aa109d915709061b0da6d1bbd0
kxxhyorim <rlagyfla0709@gmail.com>
2023-07-12 10:55:11 +0900

    [CHORE] BaseBottomSheetFragment에서 isDraggable behavior 제거

--- feature/auth/src/main/java/com/lgtm/android/auth/ui/github/GithubBottomSheet.kt
+++ feature/auth/src/main/java/com/lgtm/android/auth/ui/github/GithubBottomSheet.kt
@@ -3,6 +3,7 @@ package com.lgtm.android.auth.ui.github
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.lgtm.android.auth.BuildConfig.DEBUG
 import com.lgtm.android.auth.BuildConfig.LGTM_BASE_URL_DEBUG
 import com.lgtm.android.auth.BuildConfig.LGTM_BASE_URL_RELEASE
@@ -19,6 +20,13 @@ class GithubBottomSheet constructor(private val loginSuccessListener: OnLoginSuc
         super.onViewCreated(view, savedInstanceState)
         loadGithubLoginUsingWebView()
         setBottomSheetHeight(0.93)
+    }
+
+    override fun setBottomSheetBehavior() {
+        super.setBottomSheetBehavior()
+        (dialog as BottomSheetDialog).behavior.apply {
+            isDraggable = false
+        }
     }
 
     @SuppressLint("SetJavaScriptEnabled")
