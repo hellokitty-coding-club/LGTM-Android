8b94c84df97b16f1b22e3480bcd3fa74de86866c
kxxhyorim <rlagyfla0709@gmail.com>
2023-07-12 14:33:35 +0900

    [UI] 이용약관 화면 디자인 적용 (#22)

--- common-ui/src/main/java/com/lgtm/android/common_ui/base/BindingAdapter.kt
+++ common-ui/src/main/java/com/lgtm/android/common_ui/base/BindingAdapter.kt
@@ -1,17 +1,2 @@
 package com.lgtm.android.common_ui.base
 
-import androidx.appcompat.widget.AppCompatButton
-import androidx.databinding.BindingAdapter
-import com.lgtm.android.common_ui.R
-
-@BindingAdapter("app:btnBackground")
-fun AppCompatButton.setBtnBackground(isEnabled: Boolean) {
-    this.setBackgroundResource(
-        if (isEnabled) R.drawable.rectangle_lgtm_black else R.drawable.rectangle_lgtm_gray
-    )
-}
-
-@BindingAdapter("app:btnEnabled")
-fun AppCompatButton.setBtnEnable(isEnabled: Boolean) {
-    this.isEnabled = isEnabled
-}
