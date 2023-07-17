f6277db78f5a131a60b05c3bdb87658a859fc101
kxxhyorim <rlagyfla0709@gmail.com>
2023-07-17 15:57:20 +0900

    [REFACTOR] LGTMEditText class (#22)

--- common-ui/src/main/java/com/lgtm/android/common_ui/ui/LGTMEditText.kt
+++ common-ui/src/main/java/com/lgtm/android/common_ui/ui/LGTMEditText.kt
@@ -1,10 +1,8 @@
 package com.lgtm.android.common_ui.ui
 
 import android.annotation.SuppressLint
-import android.content.ContentValues.TAG
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -12,9 +10,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.lgtm.android.common_ui.R
 import com.lgtm.android.common_ui.databinding.LayoutLgtmEditTextBinding
 import com.lgtm.android.common_ui.model.EditTextData
+import com.lgtm.android.common_ui.model.InfoType
 
 @SuppressLint("ViewConstructor")
 class LGTMEditText @JvmOverloads constructor(
@@ -24,55 +22,67 @@ class LGTMEditText @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     private lateinit var editTextData: LiveData<EditTextData>
+
+    private val binding: LayoutLgtmEditTextBinding by lazy {
+        LayoutLgtmEditTextBinding.inflate(LayoutInflater.from(context), this, false)
+    }
+
+    init {
+        initializeView()
+        setListeners()
+        observeEditText()
+    }
+
     fun setEditTextData(editTextData: EditTextData) {
-        Log.d(TAG, "address receive: $editTextData")
         this.editTextData = MutableLiveData(editTextData)
         binding.editTextData = editTextData
     }
 
-    private val binding: LayoutLgtmEditTextBinding by lazy {
-//        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-//        DataBindingUtil.inflate(inflater, R.layout.layout_lgtm_edit_text, this, false)
-        LayoutLgtmEditTextBinding.bind(
-            LayoutInflater.from(context).inflate(R.layout.layout_lgtm_edit_text, this, false)
-        )
-    }
-
-    init {
+    private fun initializeView() {
         addView(binding.root)
         showClearButton()
+    }
+
+    private fun setListeners() {
         binding.editText.addTextChangedListener { showClearButton() }
-        observeEditText()
-//        observeEditTextData()
     }
 
     private fun showClearButton() {
-        if (binding.editText.text.isNotEmpty()) {
-            binding.ivClear.visibility = VISIBLE
-        } else {
-            binding.ivClear.visibility = GONE
-        }
+        binding.ivClear.visibility =
+            if (binding.editText.text.isNotEmpty()) View.VISIBLE else View.GONE
     }
 
     private fun observeEditText() {
+        if (::editTextData.isInitialized) return
         binding.editText.addTextChangedListener {
-            binding.tvCurrentLength.text = "${it?.length ?: 0}"
-            binding.clInfo.visibility =
-                if (editTextData.value?.infoStatus?.value?.isVisible == true) View.VISIBLE else View.GONE
-            editTextData.value?.infoStatus?.value?.icon?.let { icon ->
-                binding.icInfo.setImageResource(
-                    icon
-                )
-            }
-            editTextData.value?.infoStatus?.value?.message?.let { text ->
-                binding.tvInfo.text = text
-            }
-            editTextData.value?.infoStatus?.value?.color?.let { color ->
-                binding.tvInfo.setTextColor(ContextCompat.getColor(this.context, color))
-            }
-            // LiveData 변경
-            Log.d(TAG, "observeEditText: ${binding.editTextData?.infoStatus?.value}")
+            val infoStatus = editTextData.value?.infoStatus?.value
+            updateCurrentLength(it?.length ?: 0)
+            updateInfoVisibility(infoStatus)
+            updateInfoIcon(infoStatus)
+            updateInfoMessage(infoStatus)
+            updateInfoTextColor(infoStatus)
         }
     }
 
+    private fun updateCurrentLength(length: Int) {
+        binding.tvCurrentLength.text = "$length"
+    }
+
+    private fun updateInfoVisibility(infoStatus: InfoType?) {
+        binding.clInfo.visibility = if (infoStatus?.isVisible == true) View.VISIBLE else View.GONE
+    }
+
+    private fun updateInfoIcon(infoStatus: InfoType?) {
+        infoStatus?.icon?.let { icon -> binding.icInfo.setImageResource(icon) }
+    }
+
+    private fun updateInfoMessage(infoStatus: InfoType?) {
+        infoStatus?.message?.let { text -> binding.tvInfo.text = text }
+    }
+
+    private fun updateInfoTextColor(infoStatus: InfoType?) {
+        infoStatus?.color?.let { color ->
+            binding.tvInfo.setTextColor(ContextCompat.getColor(context, color))
+        }
+    }
 }
