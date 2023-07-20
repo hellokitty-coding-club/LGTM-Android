2e650a3bb3886752ae2f13437c76f70ec62bb43f
kxxhyorim <rlagyfla0709@gmail.com>
2023-07-20 17:17:40 +0900

    [UI] ChooseRoleFragment / 리뷰이 버튼 UI 작업 (#31)

--- common-ui/src/main/java/com/lgtm/android/common_ui/base/BindingAdapter.kt
+++ common-ui/src/main/java/com/lgtm/android/common_ui/base/BindingAdapter.kt
@@ -1,9 +1,11 @@
 package com.lgtm.android.common_ui.base
 
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.lgtm.android.common_ui.R
 
 @BindingAdapter("infoStatusColor")
 fun TextView.setInfoStatusColor(color: Int) {
@@ -13,4 +15,31 @@ fun TextView.setInfoStatusColor(color: Int) {
 @BindingAdapter("infoStatusIcon")
 fun AppCompatImageView.setInfoStatusIcon(icon: Int) {
     setImageResource(icon)
+}
+
+@BindingAdapter("setRoleImage")
+fun AppCompatImageView.setImageBySelector(isSelected: Boolean) {
+    if (isSelected) {
+        setImageResource(R.drawable.ic_reviewee_white)
+    } else {
+        setImageResource(R.drawable.ic_reviewee_green)
+    }
+}
+
+@BindingAdapter("setRoleTextColor")
+fun TextView.setTextColor(isSelected: Boolean) {
+    if (isSelected) {
+        setTextColor(ContextCompat.getColor(this.context, R.color.green))
+    } else {
+        setTextColor(ContextCompat.getColor(this.context, R.color.black))
+    }
+}
+
+@BindingAdapter("setRoleBackground")
+fun View.setBackgroundColor(isSelected: Boolean) {
+    if (isSelected) {
+        setBackgroundResource(R.drawable.rectangle_midnight_radius_10)
+    } else {
+        setBackgroundResource(R.drawable.rectangle_border_gray2_radius_10)
+    }
 }
