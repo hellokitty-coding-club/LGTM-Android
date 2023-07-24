4d46511decef2db54762d102d49c851c894327f6
kxxhyorim <rlagyfla0709@gmail.com>
2023-07-24 18:41:06 +0900

    [ADD] CompanyNameFragment 추가 (#40)

---
+++ feature/auth/src/main/java/com/lgtm/android/auth/ui/signup/reviewer/CompanyNameFragment.kt
@@ -0,0 +1,33 @@
+package com.lgtm.android.auth.ui.signup.reviewer
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import com.lgtm.android.auth.R
+import com.lgtm.android.auth.databinding.FragmentCompanyNameBinding
+import com.lgtm.android.auth.ui.signup.SignUpViewModel
+import com.lgtm.android.common_ui.base.BaseFragment
+
+class CompanyNameFragment :
+    BaseFragment<FragmentCompanyNameBinding>(R.layout.fragment_company_name) {
+    private val signUpViewModel by activityViewModels<SignUpViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupViewModel()
+        setupNextButtonListener()
+    }
+
+    private fun setupViewModel() {
+        binding.viewModel = signUpViewModel
+    }
+
+    private fun setupNextButtonListener() {
+        binding.btnNext.setOnClickListener {
+            navigateToPositionFragment()
+        }
+    }
+
+    private fun navigateToPositionFragment() {
+        // 직책 입력 화면으로 이동
+    }
+}
