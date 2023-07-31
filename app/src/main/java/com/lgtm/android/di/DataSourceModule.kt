b0e10c6d303e9c0a40716ff8da8f891abae19bd8
kxxhyorim <rlagyfla0709@gmail.com>
2023-07-31 15:34:57 +0900

    [FEAT] 로그인시 memberData 저장 / DataSource 로직 작성 (#50)

--- app/src/main/java/com/lgtm/android/di/DataSourceModule.kt
+++ app/src/main/java/com/lgtm/android/di/DataSourceModule.kt
@@ -1,6 +1,8 @@
 package com.lgtm.android.di
 
+import android.content.SharedPreferences
 import com.lgtm.android.data.datasource.IntroDataSource
+import com.lgtm.android.data.datasource.LgtmPreferenceDataSource
 import com.lgtm.android.data.service.IntroService
 import dagger.Module
 import dagger.Provides
@@ -11,11 +13,21 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RemoteDataSourceModule {
+object DataSourceModule {
 
     @Provides
     @Singleton
     fun provideIntroDataSource(introService: IntroService): IntroDataSource {
         return IntroDataSource(introService)
     }
+
+    @Provides
+    @Singleton
+    fun provideLgtmPreferenceDataSource(
+        @DefaultPreference sharedPreferences: SharedPreferences,
+        @EncryptedPreference encryptedSharedPreferences: SharedPreferences
+    ): LgtmPreferenceDataSource {
+        return LgtmPreferenceDataSource(sharedPreferences, encryptedSharedPreferences)
+    }
+
 }
