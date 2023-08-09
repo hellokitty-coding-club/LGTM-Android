56960a07a75f5a20861e3475434b64392259c71d
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-09 16:54:31 +0900

    [FEAT] MainActivity viewpager & Bottom navi sync (#66)

---
+++ feature/main/src/main/java/com/lgtm/android/main/adapter/MainViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.lgtm.android.main.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+    private val _fragmentList = mutableListOf<Fragment>()
+    var fragmentList: List<Fragment> = _fragmentList
+        set(value) {
+            _fragmentList.addAll(value)
+        }
+
+    override fun getItemCount(): Int = _fragmentList.size
+    override fun createFragment(position: Int): Fragment = _fragmentList[position]
+}
