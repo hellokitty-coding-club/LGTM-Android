a0cd71cfd26c9b3585efa702fa10bdb6e9dea329
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-09 16:38:50 +0900

    [ADD] MainActivity에 들어가는 빈 Frgment 초기화 (#66)

---
+++ feature/main/src/main/java/com/lgtm/android/main/home/HomeFragment.kt
@@ -0,0 +1,11 @@
+package com.lgtm.android.main.home
+
+import com.lgtm.android.common_ui.base.BaseFragment
+import com.lgtm.android.main.R
+import com.lgtm.android.main.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun initializeViewModel() {
+        TODO("Not yet implemented")
+    }
+}
