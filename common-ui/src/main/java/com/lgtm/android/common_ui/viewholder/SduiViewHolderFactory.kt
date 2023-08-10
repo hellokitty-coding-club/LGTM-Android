1d10d3f128fe51b8c43c420b63134f16ac4f1ae8
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-10 20:50:22 +0900

    [FEAT] SduiViewHolderFactory 구현 (#71)

---
+++ common-ui/src/main/java/com/lgtm/android/common_ui/viewholder/SduiViewHolderFactory.kt
@@ -0,0 +1,40 @@
+package com.lgtm.android.common_ui.viewholder
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.lgtm.android.common_ui.R
+import com.lgtm.android.common_ui.databinding.ItemSduiCloserBinding
+import com.lgtm.android.common_ui.databinding.ItemSduiEmptyBinding
+import com.lgtm.android.common_ui.databinding.ItemSduiItemBinding
+import com.lgtm.android.common_ui.databinding.ItemSduiTitleBinding
+import com.lgtm.android.common_ui.databinding.ItemSduiUnknownBinding
+import com.lgtm.domain.server_drive_ui.SduiViewType
+
+fun getViewHolder(parent: ViewGroup, viewType: SduiViewType): SduiBaseHolder {
+
+    val layout = getLayoutByViewType(viewType)
+    val binding: ViewDataBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(parent.context), layout, parent, false
+    )
+
+    return when (viewType) {
+        SduiViewType.TITLE -> SduiTitleViewHolder(binding as ItemSduiTitleBinding)
+        SduiViewType.ITEM -> SduiItemViewHolder(binding as ItemSduiItemBinding)
+        SduiViewType.CLOSER -> SduiCloserViewHolder(binding as ItemSduiCloserBinding)
+        SduiViewType.EMPTY -> SduiEmptyViewHolder(binding as ItemSduiEmptyBinding)
+        SduiViewType.UNKNOWN -> SduiUnknownViewHolder(binding as ItemSduiUnknownBinding)
+    }
+}
+
+private fun getLayoutByViewType(viewType: SduiViewType): Int {
+    return when (viewType) {
+        SduiViewType.TITLE -> R.layout.item_sdui_title
+        SduiViewType.ITEM -> R.layout.item_sdui_item
+        SduiViewType.CLOSER -> R.layout.item_sdui_closer
+        SduiViewType.EMPTY -> R.layout.item_sdui_empty
+        SduiViewType.UNKNOWN -> R.layout.item_sdui_unknown
+    }
+
+}
