020bc34ddf4a90955799c68fd9279c498efe7f3a
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-10 15:35:12 +0900

    [UI] 홈화면 상단 notification center 영역 추가 (#71)

--- feature/main/src/main/java/com/lgtm/android/main/home/HomeFragment.kt
+++ feature/main/src/main/java/com/lgtm/android/main/home/HomeFragment.kt
@@ -1,5 +1,8 @@
 package com.lgtm.android.main.home
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.lgtm.android.common_ui.base.BaseFragment
 import com.lgtm.android.main.R
 import com.lgtm.android.main.databinding.FragmentHomeBinding
@@ -8,4 +11,16 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun initializeViewModel() {
         TODO("Not yet implemented")
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setUpNotificationClickListener()
+    }
+
+    private fun setUpNotificationClickListener() {
+        binding.ivNotification.setOnClickListener {
+            Toast.makeText(requireContext(), "서비스 준비 중 입니다 :)", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
