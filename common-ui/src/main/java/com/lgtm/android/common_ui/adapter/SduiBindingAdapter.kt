83a342a21c31d06f8a9817c6795c3abe7d972422
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-13 03:00:00 +0900

    [FEAT] sdui용 item에 bindingAdapter 적용 (#75)

--- common-ui/src/main/java/com/lgtm/android/common_ui/adapter/SduiBindingAdapter.kt
+++ common-ui/src/main/java/com/lgtm/android/common_ui/adapter/SduiBindingAdapter.kt
@@ -6,32 +6,44 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.lgtm.domain.server_drive_ui.SduiTheme
 
+@BindingAdapter("setTitleLayerBackground")
+fun View.setTitleLayerBackground(theme: SduiTheme) {
+    when (theme) {
+        SduiTheme.LIGHT -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_gray_2_radius_top_20)
+        SduiTheme.DARK -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_gray_3_radius_top_20)
+    }
+}
 
-@BindingAdapter("setLayerBackground")
-fun View.setBackground(theme: SduiTheme) {
+
+@BindingAdapter("setItemLayerBackground")
+fun View.setItemLayerBackground(theme: SduiTheme) {
     when (theme) {
         SduiTheme.LIGHT -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_1_gray_2_side)
         SduiTheme.DARK -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_1_gray_3_side)
     }
 }
 
+@BindingAdapter("setCloserLayerBackground")
+fun View.setCloserLayerBackground(theme: SduiTheme) {
+    when (theme) {
+        SduiTheme.LIGHT -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_gray_2_radius_bottom_20)
+        SduiTheme.DARK -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_gray_3_radius_bottom_20)
+    }
+}
+
+@BindingAdapter("setDividerColor")
 fun View.setDividerColor(theme: SduiTheme) {
     when (theme) {
         SduiTheme.LIGHT -> setBackgroundColor(
-            ContextCompat.getColor(
-                this.context,
-                com.lgtm.android.common_ui.R.color.gray_2
-            )
+            ContextCompat.getColor(this.context, com.lgtm.android.common_ui.R.color.gray_2)
         )
 
         SduiTheme.DARK -> setBackgroundColor(
-            ContextCompat.getColor(
-                this.context,
-                com.lgtm.android.common_ui.R.color.gray_3
-            )
+            ContextCompat.getColor(this.context, com.lgtm.android.common_ui.R.color.gray_3)
         )
     }
 }
+
 
 @BindingAdapter("setEmptyIcon")
 fun AppCompatImageView.setEmptyImage(theme: SduiTheme) {
