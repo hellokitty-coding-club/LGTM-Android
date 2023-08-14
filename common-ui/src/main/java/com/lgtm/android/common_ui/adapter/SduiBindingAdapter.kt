381a159bba56a8e0c72813fe72d10544b9865ec2
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-14 14:32:53 +0900

    [UI] Sdui Theme 수정 (#75)

--- common-ui/src/main/java/com/lgtm/android/common_ui/adapter/SduiBindingAdapter.kt
+++ common-ui/src/main/java/com/lgtm/android/common_ui/adapter/SduiBindingAdapter.kt
@@ -9,8 +9,8 @@ import com.lgtm.domain.server_drive_ui.SduiTheme
 @BindingAdapter("setTitleLayerBackground")
 fun View.setTitleLayerBackground(theme: SduiTheme) {
     when (theme) {
-        SduiTheme.LIGHT -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_gray_2_radius_top_20)
-        SduiTheme.DARK -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_gray_3_radius_top_20)
+        SduiTheme.WHITE -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_gray_2_radius_top_20)
+        SduiTheme.GRAY -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_gray_3_radius_top_20)
     }
 }
 
@@ -18,27 +18,27 @@ fun View.setTitleLayerBackground(theme: SduiTheme) {
 @BindingAdapter("setItemLayerBackground")
 fun View.setItemLayerBackground(theme: SduiTheme) {
     when (theme) {
-        SduiTheme.LIGHT -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_1_gray_2_side)
-        SduiTheme.DARK -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_1_gray_3_side)
+        SduiTheme.WHITE -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_1_gray_2_side)
+        SduiTheme.GRAY -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_1_gray_3_side)
     }
 }
 
 @BindingAdapter("setCloserLayerBackground")
 fun View.setCloserLayerBackground(theme: SduiTheme) {
     when (theme) {
-        SduiTheme.LIGHT -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_gray_2_radius_bottom_20)
-        SduiTheme.DARK -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_gray_3_radius_bottom_20)
+        SduiTheme.WHITE -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_white_stroke_gray_2_radius_bottom_20)
+        SduiTheme.GRAY -> setBackgroundResource(com.lgtm.android.common_ui.R.drawable.rectangle_gray_2_stroke_gray_3_radius_bottom_20)
     }
 }
 
 @BindingAdapter("setDividerColor")
 fun View.setDividerColor(theme: SduiTheme) {
     when (theme) {
-        SduiTheme.LIGHT -> setBackgroundColor(
+        SduiTheme.WHITE -> setBackgroundColor(
             ContextCompat.getColor(this.context, com.lgtm.android.common_ui.R.color.gray_2)
         )
 
-        SduiTheme.DARK -> setBackgroundColor(
+        SduiTheme.GRAY -> setBackgroundColor(
             ContextCompat.getColor(this.context, com.lgtm.android.common_ui.R.color.gray_3)
         )
     }
@@ -48,7 +48,7 @@ fun View.setDividerColor(theme: SduiTheme) {
 @BindingAdapter("setEmptyIcon")
 fun AppCompatImageView.setEmptyImage(theme: SduiTheme) {
     when (theme) {
-        SduiTheme.LIGHT -> setImageResource(com.lgtm.android.common_ui.R.drawable.img_home_empty_white)
-        SduiTheme.DARK -> setImageResource(com.lgtm.android.common_ui.R.drawable.img_home_empty_gray)
+        SduiTheme.WHITE -> setImageResource(com.lgtm.android.common_ui.R.drawable.img_home_empty_white)
+        SduiTheme.GRAY -> setImageResource(com.lgtm.android.common_ui.R.drawable.img_home_empty_gray)
     }
 }
