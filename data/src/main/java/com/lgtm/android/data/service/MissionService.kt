c2520fd8b45b56774005dc212b5b6d98a5499eab
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-21 22:37:29 +0900

    [FEAT] 미션생성 / Service, DataSource (#95)

--- data/src/main/java/com/lgtm/android/data/service/MissionService.kt
+++ data/src/main/java/com/lgtm/android/data/service/MissionService.kt
@@ -1,14 +1,23 @@
 package com.lgtm.android.data.service
 
 import com.lgtm.android.data.model.response.BaseDTO
+import com.lgtm.android.data.model.response.PostMissionResponseDTO
 import com.lgtm.android.data.model.response.SduiDTO
+import com.lgtm.domain.entity.request.PostMissionRequestDTO
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface MissionService {
 
     @GET("v1/home")
     suspend fun getHomeMission(): Response<BaseDTO<SduiDTO>>
+
+    @POST("v1/mission")
+    suspend fun postMission(
+        @Body postMissionRequest: PostMissionRequestDTO
+    ): Response<BaseDTO<PostMissionResponseDTO>>
 
 }
 
