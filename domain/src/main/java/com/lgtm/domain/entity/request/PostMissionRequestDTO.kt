88b73916b223845af282755c06eebf8fa5d89b4d
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-21 22:37:07 +0900

    [FEAT] 미션생성 / Data Class 추가 (#95)

---
+++ domain/src/main/java/com/lgtm/domain/entity/request/PostMissionRequestDTO.kt
@@ -0,0 +1,13 @@
+package com.lgtm.domain.entity.request
+
+data class PostMissionRequestDTO(
+    val description: String,
+    val maxPeopleNumber: Int,
+    val missionRepositoryUrl: String,
+    val notRecommendTo: String?,
+    val price: Int,
+    val recommendTo: String?,
+    val registrationDueDate: String,
+    val tagList: List<String>,
+    val title: String
+)
