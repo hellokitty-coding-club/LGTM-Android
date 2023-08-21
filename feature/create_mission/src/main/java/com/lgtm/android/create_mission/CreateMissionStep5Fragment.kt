f58f135614e75da6ed7280a18bbf8350c3da52c9
kxxhyorim <rlagyfla0709@gmail.com>
2023-08-21 13:52:01 +0900

    [FEAT] Show DatePickerDialog With Custom Theme (#91)

--- feature/create_mission/src/main/java/com/lgtm/android/create_mission/CreateMissionStep5Fragment.kt
+++ feature/create_mission/src/main/java/com/lgtm/android/create_mission/CreateMissionStep5Fragment.kt
@@ -1,11 +1,15 @@
 package com.lgtm.android.create_mission
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import com.lgtm.android.common_ui.R.*
 import com.lgtm.android.common_ui.base.BaseFragment
 import com.lgtm.android.create_mission.databinding.FragmentCreateMissionStep5Binding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
 
 @AndroidEntryPoint
 class CreateMissionStep5Fragment :
@@ -19,7 +23,36 @@ class CreateMissionStep5Fragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupViewModel()
         setupNextButtonClickListener()
+        onEditTextClicked()
     }
+
+    private fun onEditTextClicked() {
+        binding.etRegistrationDueDate.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    val onDateClicked = DatePickerDialog.OnDateSetListener { _, year, month, day ->
+        val formattedMonth = String.format("%02d", month + 1)
+        val formattedDay = String.format("%02d", day)
+        binding.etRegistrationDueDate.setText("$year.$formattedMonth.$formattedDay")
+        //viewmodel
+    }
+
+    private fun showDatePickerDialog() {
+        val calendar = Calendar.getInstance() // To Get Today's date
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            requireContext(), style.ThemeOverlay_App_DatePicker, onDateClicked,
+            currentYear, currentMonth, currentDay
+        )
+        datePickerDialog.show()
+    }
+
 
     private fun setupViewModel() {
         binding.viewModel = createMissionViewModel
