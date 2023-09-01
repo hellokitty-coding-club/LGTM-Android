99ffce891950f4989151ff04e9e30c634068be22
kxxhyorim <rlagyfla0709@gmail.com>
2023-09-01 23:03:08 +0900

    [FEAT] MissionDetail - presentation layer (#104)

--- feature/mission_detail/src/main/java/com/lgtm/android/mission_detail/MissionDetailActivity.kt
+++ feature/mission_detail/src/main/java/com/lgtm/android/mission_detail/MissionDetailActivity.kt
@@ -1,20 +1,46 @@
 package com.lgtm.android.mission_detail
 
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.activity.viewModels
 import com.lgtm.android.common_ui.base.BaseActivity
 import com.lgtm.android.mission_detail.databinding.ActivityMissionDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.properties.Delegates
 
 
 @AndroidEntryPoint
-class MissionDetailActivity : BaseActivity<ActivityMissionDetailBinding>(R.layout.activity_mission_detail) {
+class MissionDetailActivity :
+    BaseActivity<ActivityMissionDetailBinding>(R.layout.activity_mission_detail) {
     private val missionDetailViewModel by viewModels<MissionDetailViewModel>()
-
+    private var missionId by Delegates.notNull<Int>()
     override fun initializeViewModel() {
         viewModel = missionDetailViewModel
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getExtraData()
+        getMissionDetail()
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+        getMissionDetail()
+    }
+
+    private fun getExtraData() {
+        missionId = intent.getIntExtra(MISSION_ID, defaultValue)
+    }
+
+    private fun getMissionDetail() {
+        missionDetailViewModel.getMissionDetail(missionId)
+    }
+
     companion object {
         const val MISSION_ID = "mission_id"
+        const val defaultValue = -1
     }
 }
--- feature/mission_detail/src/main/java/com/lgtm/android/mission_detail/MissionDetailViewModel.kt
+++ feature/mission_detail/src/main/java/com/lgtm/android/mission_detail/MissionDetailViewModel.kt
@@ -1,11 +1,20 @@
 package com.lgtm.android.mission_detail
 
+import androidx.lifecycle.viewModelScope
 import com.lgtm.android.common_ui.base.BaseViewModel
 import com.lgtm.domain.repository.MissionRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MissionDetailViewModel @Inject constructor(
     private val missionRepository: MissionRepository
-) : BaseViewModel()
+) : BaseViewModel(){
+
+    fun getMissionDetail(missionId: Int) {
+        viewModelScope.launch(lgtmErrorHandler) {
+            missionRepository.getMissionDetail(missionId)
+        }
+    }
+}
