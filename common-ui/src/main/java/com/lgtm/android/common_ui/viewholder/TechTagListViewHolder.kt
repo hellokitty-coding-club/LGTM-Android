4290c3363fc25f3c2e93bbf44e74413b0a98b31e
kxxhyorim <rlagyfla0709@gmail.com>
2023-09-27 22:32:31 +0900

    [FEAT] Profile 화면 - TechStack RecyclerView 구현 완료 (#124)

--- common-ui/src/main/java/com/lgtm/android/common_ui/viewholder/TechTagListViewHolder.kt
+++ common-ui/src/main/java/com/lgtm/android/common_ui/viewholder/TechTagListViewHolder.kt
@@ -1,12 +1,39 @@
 package com.lgtm.android.common_ui.viewholder
 
+import com.google.android.flexbox.FlexWrap
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.lgtm.android.common_ui.adapter.TechTagAdapter
 import com.lgtm.android.common_ui.databinding.ItemTechTagListBinding
 import com.lgtm.domain.profile.Profile
+import com.lgtm.domain.profile.TechTagList
 
 class TechTagListViewHolder(
     private val binding: ItemTechTagListBinding
 ) : ProfileBaseHolder(binding) {
-    override fun bind(profileInfo: Profile) {
-        // todo : recyclerview 구성
+    private lateinit var data: Profile
+    private lateinit var techTagAdapter: TechTagAdapter
+    override fun bind(data: Profile) {
+        this.data = data
+        initAdapter()
+        setRecyclerViewLayoutManager()
+        submitList()
     }
+
+    private fun submitList() {
+        val techTagList = (data as TechTagList).techTagList
+        techTagAdapter.submitList(techTagList)
+    }
+
+    private fun initAdapter() {
+        techTagAdapter = TechTagAdapter()
+        binding.rvTechTag.adapter = techTagAdapter
+    }
+
+    private fun setRecyclerViewLayoutManager() {
+        val layoutManager = FlexboxLayoutManager(binding.root.context).apply {
+            flexWrap = FlexWrap.WRAP
+        }
+        binding.rvTechTag.layoutManager = layoutManager
+    }
+
 }
