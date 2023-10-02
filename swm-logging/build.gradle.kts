5c05a0103f9661bcde46fc51992711118fe8343d
kxxhyorim <rlagyfla0709@gmail.com>
2023-10-02 18:50:36 +0900

    [FEAT] network library 의존성 추가 (#125)

--- swm-logging/build.gradle.kts
+++ swm-logging/build.gradle.kts
@@ -8,3 +8,15 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
 }
+
+dependencies {
+    implementation("com.google.code.gson:gson:2.8.5")
+
+    implementation("com.squareup.okhttp3:okhttp:3.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.9.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.3.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.3.0")
+}
