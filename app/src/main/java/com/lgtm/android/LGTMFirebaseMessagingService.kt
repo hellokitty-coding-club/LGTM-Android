49beed0f6ecdaf84099d251824442e2a8460e962
kxxhyorim <rlagyfla0709@gmail.com>
2023-10-25 01:40:25 +0900

    [FEAT] data type notification 백스택 지정 및 로직 함수화 (#145)

--- app/src/main/java/com/lgtm/android/LGTMFirebaseMessagingService.kt
+++ app/src/main/java/com/lgtm/android/LGTMFirebaseMessagingService.kt
@@ -2,19 +2,22 @@ package com.lgtm.android
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
-import android.graphics.Bitmap
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.lgtm.android.common_ui.R
+import com.lgtm.android.main.MainActivity
+import com.lgtm.android.manage_mission.dashboard.DashboardActivity
 import com.lgtm.domain.firebase.LgtmMessagingService
 import com.lgtm.domain.usecase.DeviceTokenManagerUseCase
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
-
 
 @AndroidEntryPoint
 class LGTMFirebaseMessagingService : FirebaseMessagingService(), LgtmMessagingService {
@@ -30,34 +33,48 @@ class LGTMFirebaseMessagingService : FirebaseMessagingService(), LgtmMessagingSe
         super.onMessageReceived(remoteMessage)
 
         if (remoteMessage.data.isNotEmpty()) {
-            // todo 추후 디테일한 로직 처리 필요
-            val data = remoteMessage.data
-            val title = data["title"]
-            val description = data["body"]
-            generateNotification(title, description)
+            handleDataType(remoteMessage.data)
         }
 
         remoteMessage.notification?.let {
-            generateNotification(it.title!!, it.body!!)
+            handleNotificationType(it)
         }
     }
 
-    private fun generateNotification(
-        title: String?,
-        message: String?,
-        image: Bitmap? = null,
-    ) {
+    private fun handleDataType(data: Map<String, String>) {
         val requestCode = System.currentTimeMillis().toInt()
 
-        val builder = NotificationCompat.Builder(this, channelID)
+        val dashboardIntent = Intent(this, DashboardActivity::class.java)
+            .putExtra(DashboardActivity.MISSION_ID, data["missionId"]?.toInt() ?: return)
+        val mainIntent = Intent(this, MainActivity::class.java)
+
+        val stackBuilder = TaskStackBuilder.create(this)
+            .addParentStack(MainActivity::class.java)
+            .addNextIntent(mainIntent)
+            .addNextIntent(dashboardIntent)
+
+        val resultPendingIntent =
+            stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = createNotificationBuilder(data["title"], data["body"], resultPendingIntent)
+        notifyWithChannel(requestCode, builder)
+    }
+
+    private fun createNotificationBuilder(
+        title: String?,
+        message: String?,
+        contentIntent: PendingIntent?
+    ): NotificationCompat.Builder {
+        return NotificationCompat.Builder(this, channelID)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-//            .setColor(Color.argb(255, 133, 120, 255))   // havit_purple
             .setAutoCancel(true)
             .setContentTitle(title)
-            .setLargeIcon(image)
             .setStyle(NotificationCompat.BigTextStyle().bigText(message))
+            .setContentIntent(contentIntent)
+    }
 
+    private fun notifyWithChannel(requestCode: Int, builder: NotificationCompat.Builder) {
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
@@ -65,6 +82,15 @@ class LGTMFirebaseMessagingService : FirebaseMessagingService(), LgtmMessagingSe
             NotificationChannel(channelID, channelUserNotice, NotificationManager.IMPORTANCE_HIGH)
         notificationManager.createNotificationChannel(notificationChannel)
         notificationManager.notify(requestCode, builder.build())
+    }
+
+    private fun handleNotificationType(notification: RemoteMessage.Notification) {
+        val title = notification.title ?: ""
+        val message = notification.body ?: ""
+        val requestCode = System.currentTimeMillis().toInt()
+
+        val builder = createNotificationBuilder(title, message, null)
+        notifyWithChannel(requestCode, builder)
     }
 
     override fun getDeviceToken(tokenCallBack: (String?) -> Unit) {
