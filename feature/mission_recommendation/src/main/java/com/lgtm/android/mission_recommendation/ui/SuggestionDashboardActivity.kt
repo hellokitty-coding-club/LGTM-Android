962d12bd38a403eeae2c332b79852f5e340531f6
915dbfl <dbfl00915@naver.com>
2024-01-16 13:03:08 +0900

    [FEAT] getSuggestion api 적용 (#203)

--- feature/mission_recommendation/src/main/java/com/lgtm/android/mission_recommendation/ui/SuggestionDashboardActivity.kt
+++ feature/mission_recommendation/src/main/java/com/lgtm/android/mission_recommendation/ui/SuggestionDashboardActivity.kt
@@ -2,17 +2,52 @@ package com.lgtm.android.mission_recommendation.ui
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import com.lgtm.android.common_ui.adapter.MissionSuggestionAdapter
 import com.lgtm.android.common_ui.base.BaseActivity
+import com.lgtm.android.common_ui.util.setOnThrottleClickListener
 import com.lgtm.android.mission_recommendation.R
 import com.lgtm.android.mission_recommendation.databinding.ActivitySuggestionDashboardBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SuggestionDashboardActivity : BaseActivity<ActivitySuggestionDashboardBinding>(R.layout.activity_suggestion_dashboard) {
     private val suggestionDashboardViewModel by viewModels<SuggestionDashboardViewModel>()
+    private lateinit var missionSuggestionAdapter: MissionSuggestionAdapter
     override fun initializeViewModel() {
         viewModel = suggestionDashboardViewModel
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupBindingData()
+        observeMissionSuggestion()
+        initAdapter()
+        setBackButtonClickListener()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        suggestionDashboardViewModel.getSuggestion()
+    }
+
+    private fun setupBindingData() {
+        binding.viewModel = suggestionDashboardViewModel
+    }
+
+    private fun observeMissionSuggestion() {
+        suggestionDashboardViewModel.missionSuggestion.observe(this) {
+            missionSuggestionAdapter.submitList(it.suggestions)
+        }
+    }
+
+    private fun initAdapter() {
+        missionSuggestionAdapter = MissionSuggestionAdapter()
+        binding.rvMissionSuggestion.adapter = missionSuggestionAdapter
+    }
+
+    private fun setBackButtonClickListener() {
+        binding.ivBack.setOnThrottleClickListener {
+            finish()
+        }
     }
 }
--- feature/mission_recommendation/src/main/java/com/lgtm/android/mission_recommendation/ui/SuggestionDashboardViewModel.kt
+++ feature/mission_recommendation/src/main/java/com/lgtm/android/mission_recommendation/ui/SuggestionDashboardViewModel.kt
@@ -1,6 +1,38 @@
 package com.lgtm.android.mission_recommendation.ui
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.crashlytics.ktx.crashlytics
+import com.google.firebase.ktx.Firebase
 import com.lgtm.android.common_ui.base.BaseViewModel
+import com.lgtm.domain.entity.response.MissionSuggestionVO
+import com.lgtm.domain.repository.SuggestionRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class SuggestionDashboardViewModel: BaseViewModel() {
+@HiltViewModel
+class SuggestionDashboardViewModel @Inject constructor(
+    private val suggestionRepository: SuggestionRepository
+): BaseViewModel() {
+
+    private val _missionSuggestion = MutableLiveData<MissionSuggestionVO>()
+    val missionSuggestion: LiveData<MissionSuggestionVO> = _missionSuggestion
+
+    fun getSuggestion() {
+        viewModelScope.launch(lgtmErrorHandler) {
+            suggestionRepository.getSuggestion()
+                .onSuccess {
+                    _missionSuggestion.postValue(it)
+                    Log.d(TAG, "getSuggestion: $it")
+                }.onFailure {
+                    Firebase.crashlytics.recordException(it)
+                    Log.e(TAG, "getSuggestion: $it")
+                }
+        }
+    }
+
 }
