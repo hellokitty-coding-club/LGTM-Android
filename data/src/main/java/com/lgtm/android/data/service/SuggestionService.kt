2503b300425936c871dd6cf6ea33e335c9f7bdd9
915dbfl <dbfl00915@naver.com>
2024-01-31 16:13:14 +0900

    [FIX] 미션 제안 상세 조회 api에 suggeestionId를 Path로 설정해 api 호출 오류 해결 (#206)

--- data/src/main/java/com/lgtm/android/data/service/SuggestionService.kt
+++ data/src/main/java/com/lgtm/android/data/service/SuggestionService.kt
@@ -5,11 +5,14 @@ import com.lgtm.android.data.model.response.MissionSuggestionDTO
 import com.lgtm.android.data.model.response.SuggestionDTO
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface SuggestionService {
     @GET("v1/suggestion")
     suspend fun getSuggestion(): Response<BaseDTO<MissionSuggestionDTO>>
 
     @GET("v1/suggestion/{suggestionId}")
-    suspend fun getSuggestionDetail(): Response<BaseDTO<SuggestionDTO>>
+    suspend fun getSuggestionDetail(
+        @Path("suggestionId") suggestionId: Int
+    ): Response<BaseDTO<SuggestionDTO>>
 }
