e7ec1ccfc84b7057fa157a9a9a2017c21b4b8fb1
915dbfl <dbfl00915@naver.com>
2024-01-31 14:26:57 +0900

    [FEAT] LikeButton compose component 추가(로직x) (#206)

---
+++ common-ui/src/main/java/com/lgtm/android/common_ui/components/buttons/LikeButton.kt
@@ -0,0 +1,69 @@
+package com.lgtm.android.common_ui.components.buttons
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lgtm.android.common_ui.R
+import com.lgtm.android.common_ui.theme.body3R
+
+@Composable
+fun LikeButton(
+    likeCount: String,
+    isLiked: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .border(
+                width = 1.dp,
+                color = colorResource(id = R.color.gray_2),
+                shape = RoundedCornerShape(10.dp)
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .padding(
+                horizontal = 6.dp,
+                vertical = 9.dp
+            )
+    ) {
+        
+        Text(
+            text = likeCount,
+            style = Typography.body3R
+        )
+
+        Image(
+            modifier = Modifier.padding(start = 2.dp),
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_like_selected),
+            contentDescription = null
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LikeButtonPreview() {
+    MaterialTheme {
+        LikeButton(
+            likeCount = "999+",
+            isLiked = true
+        )
+    }
+}
