17fb78376150815f6c685e2b1ade7392257ad1bb
915dbfl <dbfl00915@naver.com>
2024-01-31 16:21:02 +0900

    [FEAT] SuggestionUI를 적용한 SuggestionDetailScreen 화면에 표시 (#206)

--- feature/mission_recommendation/src/main/java/com/lgtm/android/mission_recommendation/ui/detail/presentation/SuggestionDetailScreen.kt
+++ feature/mission_recommendation/src/main/java/com/lgtm/android/mission_recommendation/ui/detail/presentation/SuggestionDetailScreen.kt
@@ -8,15 +8,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.lgtm.android.common_ui.R
 import com.lgtm.android.common_ui.components.buttons.BackButton
@@ -28,7 +27,9 @@ import com.lgtm.android.common_ui.theme.body2
 import com.lgtm.android.common_ui.theme.heading3B
 
 @Composable
-fun SuggestionDetailScreen() {
+fun SuggestionDetailScreen(
+    suggestionDetailStateHolder: State<SuggestionDetailState>
+) {
     Column(
         modifier = Modifier
             .background(colorResource(id = R.color.gray_3)),
@@ -36,94 +37,112 @@ fun SuggestionDetailScreen() {
     ) {
         SuggestionDetailContent(
             modifier = Modifier
-                .weight(1f)
+                .weight(1f),
+            suggestionDetailState = suggestionDetailStateHolder.value
         )
         SuggestionLikeSection(
-            modifier = Modifier.padding(vertical = 5.dp)
+            modifier = Modifier.padding(vertical = 5.dp),
+            suggestionDetailState = suggestionDetailStateHolder.value
         )
     }
 }
 
 @Composable
 fun SuggestionDetailContent(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    suggestionDetailState: SuggestionDetailState
 ) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .background(
-                color = Color.White,
-                shape = RoundedCornerShape(
-                    bottomStart = 20.dp,
-                    bottomEnd = 20.dp
+    when (suggestionDetailState) {
+        is SuggestionDetailState.Loading -> {
+            /* no-op */
+        }
+        is SuggestionDetailState.Main -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(
+                            bottomStart = 20.dp,
+                            bottomEnd = 20.dp
+                        )
+                    )
+            ) {
+                SuggestionDetailTopBar()
+
+                Text(
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    text = suggestionDetailState.suggestionDetail.title,
+                    style = Typography.heading3B,
+                    color = Color.Black
                 )
-            )
-    ) {
-        SuggestionDetailTopBar()
 
-        Text(
-            modifier = Modifier.padding(horizontal = 20.dp),
-            text = "확인용 텍스트 확인용 텍스트확인용 텍스트",
-            style = Typography.heading3B,
-            color = Color.Black
-        )
+                DateTimeText(
+                    date = suggestionDetailState.suggestionDetail.date,
+                    time = suggestionDetailState.suggestionDetail.time,
+                    modifier = Modifier.padding(
+                        vertical = 11.dp,
+                        horizontal = 20.dp
+                    )
+                )
 
-        DateTimeText(
-            date = "2021.01.01",
-            time = "오후 12:00",
-            modifier = Modifier.padding(
-                vertical = 11.dp,
-                horizontal = 20.dp
-            )
-        )
+                Divider(
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    color = colorResource(id = R.color.gray_2),
+                    thickness = 1.dp
+                )
 
-        Divider(
-            modifier = Modifier.padding(horizontal = 20.dp),
-            color = colorResource(id = R.color.gray_2),
-            thickness = 1.dp
-        )
-
-        Text(
-            modifier = Modifier.padding(
-                top = 12.dp,
-                start = 20.dp,
-                end = 20.dp
-            ),
-            text = "집앞에는 롯데리아뿐이고... 프랜차이즈 다 맛대가리 없고...집앞에는 롯데리아뿐이고... 프랜차이즈 다 맛대가리 없고...집앞에는 롯데리아뿐이고... 프랜차이즈 다 맛대가리 없고...집앞에는 롯데리아뿐이고... 프랜차이즈 다 맛대가리 없고...집앞에는 롯데리아뿐이고... 프랜차이즈 다 맛대가리 없고...집앞에는 롯데리아뿐이고... 프랜차이즈 다 맛대가리 없고...",
-            style = Typography.body2,
-            color = Color.Black
-        )
+                Text(
+                    modifier = Modifier.padding(
+                        top = 12.dp,
+                        start = 20.dp,
+                        end = 20.dp
+                    ),
+                    text = suggestionDetailState.suggestionDetail.description,
+                    style = Typography.body2,
+                    color = Color.Black
+                )
+            }
+        }
     }
 }
 
 @Composable
 fun SuggestionLikeSection(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    suggestionDetailState: SuggestionDetailState
 ) {
-    Row (
-        modifier = modifier
-            .fillMaxWidth()
-            .background(
-                color = colorResource(id = R.color.gray_2),
-                shape = RoundedCornerShape(20.dp)
-            )
-            .padding(
-                horizontal = 20.dp,
-                vertical = 14.dp
-            ),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween,
-    ) {
-        Text(
-            text = stringResource(id = R.string.want_this_mission),
-            style = Typography.body1B,
-            color = colorResource(id = R.color.gray_6)
-        )
+    when(suggestionDetailState) {
+        is SuggestionDetailState.Loading -> {
+            /* no-op */
+        }
+        is SuggestionDetailState.Main -> {
+            Row (
+                modifier = modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = colorResource(id = R.color.gray_2),
+                        shape = RoundedCornerShape(20.dp)
+                    )
+                    .padding(
+                        horizontal = 20.dp,
+                        vertical = 14.dp
+                    ),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = stringResource(id = R.string.want_this_mission),
+                    style = Typography.body1B,
+                    color = colorResource(id = R.color.gray_6)
+                )
 
-        LikeButton(
-            likeCount = "+999",
-            isLiked = true
-        )
+                LikeButton(
+                    likeNum = suggestionDetailState.suggestionDetail.likeNum,
+                    isLiked = suggestionDetailState.suggestionDetail.isLiked
+                )
+            }
+        }
     }
 }
 
@@ -141,14 +160,5 @@ fun SuggestionDetailTopBar() {
     ) {
         BackButton()
         MenuButton()
-    }
-}
-
-
-@Preview
-@Composable
-fun SuggestionDetailPreview() {
-    MaterialTheme {
-        SuggestionDetailScreen()
     }
 }
