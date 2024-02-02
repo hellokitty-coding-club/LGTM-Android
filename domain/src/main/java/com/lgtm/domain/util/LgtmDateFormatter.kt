5b7c816862ee96b4c8d4d3a22605add3b9a60722
kxxhyorim <rlagyfla0709@gmail.com>
2024-02-02 17:41:01 +0900

    [FEAT] DateTimeFormatter 한국 locale 명시적으로 지정 (#208)

--- domain/src/main/java/com/lgtm/domain/util/LgtmDateFormatter.kt
+++ domain/src/main/java/com/lgtm/domain/util/LgtmDateFormatter.kt
@@ -1,7 +1,9 @@
 package com.lgtm.domain.util
 
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 val dotStyleFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
 val isoStyleFormatter: DateTimeFormatter = DateTimeFormatter.ISO_DATE
-val timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("a h:mm")
+val timeFormatter: DateTimeFormatter =
+    DateTimeFormatter.ofPattern("a h:mm").withLocale(Locale("ko", "KR"))
