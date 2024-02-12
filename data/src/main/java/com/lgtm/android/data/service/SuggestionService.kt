7370467bc7a511cfb9bd213d072fea8c843f2004
915dbfl <dbfl00915@naver.com>
2024-02-12 16:09:32 +0900

    [FEAT] createSuggestion api retrofit 적용 (#204)

--- data/src/main/java/com/lgtm/android/data/service/SuggestionService.kt
+++ data/src/main/java/com/lgtm/android/data/service/SuggestionService.kt
@@ -1,10 +1,14 @@
 package com.lgtm.android.data.service
 
 import com.lgtm.android.data.model.response.BaseDTO
+import com.lgtm.android.data.model.response.CreateSuggestionResponseDTO
 import com.lgtm.android.data.model.response.MissionSuggestionDTO
 import com.lgtm.android.data.model.response.SuggestionDTO
+import com.lgtm.domain.entity.request.CreateSuggestionRequestDTO
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface SuggestionService {
@@ -15,4 +19,9 @@ interface SuggestionService {
     suspend fun getSuggestionDetail(
         @Path("suggestionId") suggestionId: Int
     ): Response<BaseDTO<SuggestionDTO>>
+
+    @POST("v1/suggestion")
+    suspend fun createSuggestion(
+        @Body createSuggestionRequestDTO: CreateSuggestionRequestDTO
+    ): Response<BaseDTO<CreateSuggestionResponseDTO>>
 }
--- domain/src/main/java/com/lgtm/domain/repository/SuggestionRepository.kt
+++ domain/src/main/java/com/lgtm/domain/repository/SuggestionRepository.kt
@@ -1,9 +1,12 @@
 package com.lgtm.domain.repository
 
+import com.lgtm.domain.entity.request.CreateSuggestionRequestDTO
+import com.lgtm.domain.entity.response.CreateSuggestionResponseVO
 import com.lgtm.domain.mission_suggestion.MissionSuggestionVO
 import com.lgtm.domain.mission_suggestion.SuggestionVO
 
 interface SuggestionRepository {
     suspend fun getSuggestion(): Result<MissionSuggestionVO>
     suspend fun getSuggestionDetail(suggestionId: Int): Result<SuggestionVO>
+    suspend fun createSuggestion(createSuggestionRequest: CreateSuggestionRequestDTO): Result<CreateSuggestionResponseVO>
 }
