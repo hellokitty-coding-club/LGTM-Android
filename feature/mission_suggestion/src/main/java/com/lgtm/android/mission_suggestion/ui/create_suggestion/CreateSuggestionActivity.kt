27429f657ff4073d740bc91f3fdee908573048f9
915dbfl <dbfl00915@naver.com>
2024-02-13 19:28:10 +0900

    [FEAT] dialogFragment 활용을 위해 createSuggestionActivity appCompatActivity 상속 (#204)

--- feature/mission_suggestion/src/main/java/com/lgtm/android/mission_suggestion/ui/create_suggestion/CreateSuggestionActivity.kt
+++ feature/mission_suggestion/src/main/java/com/lgtm/android/mission_suggestion/ui/create_suggestion/CreateSuggestionActivity.kt
@@ -2,23 +2,30 @@ package com.lgtm.android.mission_suggestion.ui.create_suggestion
 
 import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import com.lgtm.android.common_ui.base.BaseComposeActivity
+import com.lgtm.android.common_ui.base.BaseActivity
+import com.lgtm.android.mission_suggestion.R
+import com.lgtm.android.mission_suggestion.databinding.ActivityCreateSuggestionBinding
 import com.lgtm.android.mission_suggestion.ui.create_suggestion.presentation.CreateSuggestionScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CreateSuggestionActivity: BaseComposeActivity() {
+class CreateSuggestionActivity: BaseActivity<ActivityCreateSuggestionBinding>(R.layout.activity_create_suggestion) {
     private val createSuggestionViewModel by viewModels<CreateSuggestionViewModel>()
     override fun initializeViewModel() {
         viewModel = createSuggestionViewModel
     }
 
-    @Composable
-    override fun Content() {
-        MaterialTheme {
-            CreateSuggestionScreen {
-                setBackButtonClick()
+    override fun onResume() {
+        super.onResume()
+        setComposableContent()
+    }
+    
+    private fun setComposableContent() {
+        binding.composeView.setContent {
+            MaterialTheme {
+                CreateSuggestionScreen {
+                    setBackButtonClick()
+                }
             }
         }
     }
@@ -26,4 +33,5 @@ class CreateSuggestionActivity: BaseComposeActivity() {
     private fun setBackButtonClick() {
         finish()
     }
+
 }
