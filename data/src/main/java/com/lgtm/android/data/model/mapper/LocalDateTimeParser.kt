4a4b5516d566fd7c68c37e54e2bc39473e4a0460
kxxhyorim <rlagyfla0709@gmail.com>
2024-02-29 18:09:28 +0900

    [CHORE] LocalDateTimeParser 함수 parameter 네이밍 변경 (#208)

--- data/src/main/java/com/lgtm/android/data/model/mapper/LocalDateTimeParser.kt
+++ data/src/main/java/com/lgtm/android/data/model/mapper/LocalDateTimeParser.kt
@@ -5,15 +5,15 @@ import com.google.firebase.ktx.Firebase
 import java.time.LocalDateTime
 import java.time.format.DateTimeParseException
 
-fun parseDate(createdAt: String?): LocalDateTime? {
+fun parseDate(date: String?): LocalDateTime? {
     return try {
-        checkNotNull(createdAt)
-        LocalDateTime.parse(createdAt)
+        checkNotNull(date)
+        LocalDateTime.parse(date)
     } catch (e: DateTimeParseException) {
-        Firebase.crashlytics.log("$e / Wrong Date format : $createdAt")
+        Firebase.crashlytics.log("$e / Wrong Date format : $date")
         null
     } catch (e: IllegalStateException) {
-        Firebase.crashlytics.log("$e / created value might be null : $createdAt")
+        Firebase.crashlytics.log("$e / created value might be null : $date")
         null
     }
 }
